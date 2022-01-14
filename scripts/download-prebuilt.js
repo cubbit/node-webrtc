b2d07f17af05fa902054a9e5331a2da51688615b
Marco Moschettini <m.marmos@gmail.com>
2022-01-14 16:03:37 +0100

    chore: change install script

--- scripts/download-prebuilt.js
+++ scripts/download-prebuilt.js
@@ -1,10 +1,33 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 /* eslint no-process-env:0, no-process-exit:0 */
 'use strict';
 
 const { spawnSync } = require('child_process');
+const os = require('os');
+const https = require('https');
 
-function main(exit) {
+const fs = require('fs');
+const path = require('path');
+
+const modulePath = path.resolve(__dirname, '..');
+const bindingsPath = path.resolve(modulePath, 'build', 'Release', 'wrtc.node');
+
+function download(url, destination) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      const file = fs.createWriteStream(destination);
+      response.pipe(file);
+
+      file.on('finish', () => {
+        file.close();
+        resolve();
+      });
+    }).on('error', reject);
+  });
+}
+
+async function main(exit) {
   const args = ['install'];
 
   if (process.env.DEBUG) {
@@ -15,16 +38,30 @@ function main(exit) {
     args.push('--target_arch=' + process.env.TARGET_ARCH);
   }
 
-  let { status } = spawnSync('node-pre-gyp', args, {
-    shell: true,
-    stdio: 'inherit'
-  });
-  if (status) {
-    if (!exit) {
-      throw new Error(status);
+  if (os.platform() === 'darwin' && os.arch() === 'arm64') {
+    const url = 'https://cubbit.s3.eu-central-1.amazonaws.com/node-webrtc/0.4.7-dev/darwin/arm64/wrtc.node';
+
+    console.log('Apple Silicon CPU detected. Downloading prebuilt for darwin-arm64 from', url);
+    if (!fs.existsSync(bindingsPath)) {
+      fs.mkdirSync(path.dirname(bindingsPath), { recursive: true });
     }
 
-    process.exit(1);
+    await download(url, bindingsPath);
+    console.log('Downloaded prebuilt for darwin-arm64 at', bindingsPath);
+  }
+  else {
+    let { status } = spawnSync('node-pre-gyp', args, {
+      shell: true,
+      stdio: 'inherit'
+    });
+
+    if (status) {
+      if (!exit) {
+        throw new Error(status);
+      }
+
+      process.exit(1);
+    }
   }
 }
 
