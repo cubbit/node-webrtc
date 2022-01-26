0e040a0322ac3f1f932456fef562be0e7e2f79cd
Marco Moschettini <m.marmos@gmail.com>
2022-01-26 13:03:45 +0100

    fix lint

--- scripts/download-prebuilt.js
+++ scripts/download-prebuilt.js
@@ -4,28 +4,6 @@
 'use strict';
 
 const { spawnSync } = require('child_process');
-const os = require('os');
-const https = require('https');
-
-const fs = require('fs');
-const path = require('path');
-
-const modulePath = path.resolve(__dirname, '..');
-const bindingsPath = path.resolve(modulePath, 'build', 'Release', 'wrtc.node');
-
-function download(url, destination) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (response) => {
-      const file = fs.createWriteStream(destination);
-      response.pipe(file);
-
-      file.on('finish', () => {
-        file.close();
-        resolve();
-      });
-    }).on('error', reject);
-  });
-}
 
 async function main(exit) {
   const args = ['install'];
@@ -38,18 +16,6 @@ async function main(exit) {
     args.push('--target_arch=' + process.env.TARGET_ARCH);
   }
 
-  // if (os.platform() === 'darwin' && os.arch() === 'arm64') {
-  //   const url = 'https://cubbit.s3.eu-central-1.amazonaws.com/node-webrtc/0.4.7-dev/darwin/arm64/wrtc.node';
-
-  //   console.log('Apple Silicon CPU detected. Downloading prebuilt for darwin-arm64 from', url);
-  //   if (!fs.existsSync(bindingsPath)) {
-  //     fs.mkdirSync(path.dirname(bindingsPath), { recursive: true });
-  //   }
-
-  //   await download(url, bindingsPath);
-  //   console.log('Downloaded prebuilt for darwin-arm64 at', bindingsPath);
-  // }
-  // else {
   let { status } = spawnSync('node-pre-gyp', args, {
     shell: true,
     stdio: 'inherit'
@@ -62,7 +28,6 @@ async function main(exit) {
 
     process.exit(1);
   }
-  // }
 }
 
 module.exports = main;
